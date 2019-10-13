fbb4c8320d5125355b6f1935155f4443761d7310
Patrycja <bielpatrycjaa@gmail.com>
2019-10-13 12:53:25 +0200

    First endpoint

--- index.js
+++ index.js
@@ -2,4 +2,17 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('public'))
+
+app.get('/api/v1/getAllProducts', (req, res) => {
+  req.status(200).send({
+    success: 'true',
+    message: 'todos retrieved successfully',
+    products: {
+                id: 1,
+                title: "lunch",
+                description: "Go for lunc by 2pm"
+              }
+  })
+})
+
 app.listen(3000, () => console.log('Server running on port 3000'))
