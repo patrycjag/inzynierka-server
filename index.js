3c22e69f53569573d3562f9102b0c9f43662230b
lpkepka <kepka@pc8.home>
2019-10-28 20:09:59 +0100

    Added price calculation, attempt to add promises to php calls for price

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const request = require('request')
-var DomParser = require('dom-parser');
+const DomParser = require('dom-parser');
 
 app.get('/api/v1/getPricesFor/:productName', (req, res) => {
     var promise = getDataFromSkapiec(req.params.productName);
@@ -21,9 +21,9 @@ app.get('/api/v1/getOffersFor/:productId', (req, res) => {
     });
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'))
+app.listen(3000, () => console.log('Server running on port 3000'));
 
-const getDataFromSkapiec = function(query) {
+const getDataFromSkapiec = (query) => {
     return new Promise((resolve, reject) => {
 
         request("https://www.skapiec.pl/szukaj/w_calym_serwisie/" + query, function (error, response, body) {
@@ -31,7 +31,7 @@ const getDataFromSkapiec = function(query) {
             var doc = parser.parseFromString(body, "text/html");
             var boxes = doc.getElementsByClassName("box-row js");
             var jsonScripts = [];
-            Array.prototype.forEach.call(boxes, function(el) {
+            Array.prototype.forEach.call(boxes, (el) => {
                 var htmlBox = parser.parseFromString(el.innerHTML);
                 var script = htmlBox.getElementsByTagName("script");
                 if (script[0] != undefined) {
@@ -48,7 +48,7 @@ const getDataFromSkapiec = function(query) {
     });
 }
 
-const getProductDetails = function(productId) {
+const getProductDetails = (productId) => {
     return new Promise((resolve, reject) => {
         request("https://www.skapiec.pl/site/cat/200/comp/" + productId, function (error, response, body) {
             var parser = new DomParser();
@@ -58,7 +58,7 @@ const getProductDetails = function(productId) {
             var allOfferRows = parser.parseFromString(allOffers[0].innerHTML).getElementsByTagName("a");
             var bestOfferRows = parser.parseFromString(bestOffers[0].innerHTML).getElementsByTagName("a");
             var allItemOffers = [];
-            Array.prototype.forEach.call(bestOfferRows, function(el) {
+            Array.prototype.forEach.call(bestOfferRows, (el) => {
                 const priceHtml = parser.parseFromString(el.innerHTML).getElementsByClassName("section-price-value");
                 if (priceHtml[0] != undefined) {
                     var price = parser.parseFromString(priceHtml[0].innerHTML).getElementsByTagName("span")[0].textContent;
@@ -73,16 +73,22 @@ const getProductDetails = function(productId) {
                     const delivery = aHref.getElementsByTagName('a');
                     const deliverySpan = aHref.getElementsByTagName('span');
                     if (delivery[0] != undefined) {
-                        allItemOffers.push({"price" : price, "description" : description, "imgUrl" : src, "shopName" : alt, "delivery" : 10 });
+                        let phpScript = delivery[0].getAttribute('href');
+                        const deliveryCost = getDeliveryCostFromPhp(phpScript);
+                        deliveryCost.then(function(value) {
+                            allItemOffers.push({"price" : price, "description" : description, "imgUrl" : src, "shopName" : alt, "delivery" : value });
+                        }, function() {
+                            allItemOffers.push({"price" : price, "description" : description, "imgUrl" : src, "shopName" : alt, "delivery" : 'No info'});
+                        });
                     } else if (deliverySpan[0] != undefined) {
-                        allItemOffers.push({"price" : price, "description" : description, "imgUrl" : src, "shopName" : alt, "delivery" : 0});
+                        allItemOffers.push({"price" : price, "description" : description, "imgUrl" : src, "shopName" : alt, "delivery" : '0.00 zł'});
                     } else {
-                        console.log("No price info.")
+                        allItemOffers.push({"price" : price, "description" : description, "imgUrl" : src, "shopName" : alt, "delivery" : 'No info'});
                     }
                 }
             });
 
-            Array.prototype.forEach.call(allOfferRows, function(el) {
+            Array.prototype.forEach.call(allOfferRows, (el, index, array) => {
                 const priceHtml2 = parser.parseFromString(el.innerHTML).getElementsByClassName("section-price-value");
                 if (priceHtml2[0] != undefined) {
                     var price = parser.parseFromString(priceHtml2[0].innerHTML).getElementsByTagName("span")[0].textContent;
@@ -97,31 +103,113 @@ const getProductDetails = function(productId) {
                     const delivery = aHref.getElementsByTagName('a');
                     const deliverySpan = aHref.getElementsByTagName('span');
                     if (delivery[0] != undefined) {
-                        allItemOffers.push({"price" : price, "description" : description, "imgUrl" : src, "shopName" : alt, "delivery" : 10 });
+                        let phpScript = delivery[0].getAttribute('href');
+                        const deliveryCost = getDeliveryCostFromPhp(phpScript);
+                        deliveryCost.then(function(value) {
+                            allItemOffers.push({"price" : price, "description" : description, "imgUrl" : src, "shopName" : alt, "delivery" : value });
+                            if (index === array.length - 1) {
+                                if (allItemOffers.length > 0) {
+                                    resolve(allItemOffers); // fulfilled
+                                } else {
+                                    reject("No scripts to be had");
+                                }
+                            }
+                        }, function() {
+                            allItemOffers.push({"price" : price, "description" : description, "imgUrl" : src, "shopName" : alt, "delivery" : 'No info'});
+                            if (index === array.length - 1) {
+                                if (allItemOffers.length > 0) {
+                                    resolve(allItemOffers); // fulfilled
+                                } else {
+                                    reject("No scripts to be had");
+                                }
+                            }
+                        });
                     } else if (deliverySpan[0] != undefined) {
-                        allItemOffers.push({"price" : price, "description" : description, "imgUrl" : src, "shopName" : alt, "delivery" : 0});
+                        allItemOffers.push({"price" : price, "description" : description, "imgUrl" : src, "shopName" : alt, "delivery" : '0.00 zł'});
+                        if (index === array.length - 1) {
+                            if (allItemOffers.length > 0) {
+                                resolve(allItemOffers); // fulfilled
+                            } else {
+                                reject("No scripts to be had");
+                            }
+                        }
                     } else {
-                        console.log("No price info.")
+                        allItemOffers.push({"price" : price, "description" : description, "imgUrl" : src, "shopName" : alt, "delivery" : 'No info'});
+                        if (index === array.length - 1) {
+                            if (allItemOffers.length > 0) {
+                                resolve(allItemOffers); // fulfilled
+                            } else {
+                                reject("No scripts to be had");
+                            }
+                        }
                     }
                 }
             });
+        });
+    });
+}
 
-            if (allItemOffers.length > 0) {
-                resolve(allItemOffers); // fulfilled
-            } else {
-                reject("No scripts to be had");
+const getDeliveryCostFromPhp = (query) => {
+    return new Promise((resolve, reject) => {
+        request("https://www.skapiec.pl/" + query, function (error, response, body) {
+            try {
+                const parser = new DomParser();
+                const doc = parser.parseFromString(body, "text/html");
+                const tableRow = doc.getElementsByClassName('even');
+                const rowFromTableRow = parser.parseFromString(tableRow[0].innerHTML);
+                const bTag = rowFromTableRow.getElementsByTagName('b');
+                const price = bTag[0].textContent.split('\n').join('');
+                resolve(price);
+            }
+            catch(error) {
+                console.error(error);
+                reject('Could not get price');
             }
         });
     });
 }
 
-const calculateBestPriceWithoutDelivery = function() {
-    var prices = {"xkom.pl" : [220, 230, 500], "emag.pl" : [320, 330, 510], "morele.net" : [250, 247, 543], "empik.pl" : [199, 220, 430]}
-    var lowestPriceFromSameShop = 0
-    Object.keys(prices).forEach(function(key) {
-        var value = prices[key];
-        Array.prototype.forEach.call(value, function (singleValue) {
-
-        });
+const calculateBestPriceFromOneShop = () => {
+    const prices1 = {"xkom.pl" : [220, 230, 500], "emag.pl" : [220, 210, 510], "morele.net" : [220, 247, 543], "empik.pl" : [220, 220, 430]}
+    var lowestPrice = 0;
+    var selectedShop;
+    Object.entries(prices1).forEach( (value, index) => {
+        const key = value[0];
+        const prices = value[1];
+        const sum = prices.reduce((a, b) => a + b);
+        if (index == 0 || lowestPrice > sum) {
+            lowestPrice = sum;
+            selectedShop = key;
+        }
     });
+    console.log(lowestPrice);
+    console.log(selectedShop);
 }
+
+const calculateBestPriceFromDifferentShops = () => {
+    const prices1 = {"xkom.pl" : [220, 230, 500], "emag.pl" : [220, 210, 510], "morele.net" : [220, 247, 543], "empik.pl" : [220, 220, 430]}
+    var lowestPrice = [];
+    var selectedShops = [];
+    const firstShop = Object.entries(prices1)[0];
+    firstShop[1].forEach((price) => {
+        selectedShops.push(firstShop[0]);
+        lowestPrice.push(price);
+    });
+    const numberOfShops = Object.entries(prices1).length;
+    for (let i = 1; i < numberOfShops; i++) {
+        const shop = Object.entries(prices1)[i];
+        shop[1].forEach((price2, index) => {
+            if (lowestPrice[index] > price2) {
+                lowestPrice[index] = price2;
+                selectedShops[index] = shop[0];
+            }
+        });
+    }
+
+    const totalPrice = lowestPrice.reduce((a, b) => a + b);
+
+    console.log(lowestPrice);
+    console.log(selectedShops);
+    console.log(totalPrice);
+}
+
